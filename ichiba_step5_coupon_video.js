98e2a4a902aae61d494a973d84d9ae00d30f25bd
testfordfp3 <62692779+testfordfp3@users.noreply.github.com>
2020-04-07 14:36:11 +0900

    Add files via upload

---
+++ ichiba_step5_coupon_video.js
@@ -0,0 +1,13 @@
+(function() {
+	if ((window.navigator.userAgent.indexOf('MSIE 7.0') < 0)
+			&& (window.navigator.userAgent.indexOf('MSIE 8.0') < 0)
+			&& (window.navigator.userAgent.indexOf('MSIE 9.0') < 0)
+			&& (window.navigator.userAgent.indexOf('Android 2.') < 0)
+			&& (window.navigator.userAgent.indexOf('NetReader') < 0)) {
+
+		var ichiba_step5_coupon_frame_js = document.createElement('script');
+		ichiba_step5_coupon_frame_js.type = 'text/javascript';
+		ichiba_step5_coupon_frame_js.src = '/ichiba_step5_coupon_video_frame.js?v=1.0';
+		document.body.appendChild(ichiba_step5_coupon_frame_js);
+	}
+})(this);
---
+++ ichiba_step5_coupon_video_frame_multiple_sp.js
@@ -0,0 +1,168 @@
+(function($) {
+    //
+    // Escape Codec Library: ecl.js (Ver.041208)
+    //
+    // Copyright (C) http://nurucom-archives.hp.infoseek.co.jp/digital/
+    //
+    var UnescapeEUCJP=function(str){
+        return str.replace(/(%A[1-9A-F]|%[B-E][0-9A-F]|%F[0-9A-E]){2}|%8E%(A[1-9A-F]|[B-D][0-9A-F])|%[0-7][0-9A-F]/ig,function(s){
+            var c=parseInt(s.substring(1),16);
+            return c<161?String.fromCharCode(c<128?c:parseInt(s.substring(4),16)+65216):JCT8836.charAt((c-161)*94+parseInt(s.substring(4),16)-161)
+        })
+    };
+    var JCT11280=Function('var a=\'zKV33~jZ4zN=~ji36XazM93y!{~k2y!o~k0ZlW6zN?3Wz3W?{EKzK[33[`y|;-~j^YOTz$!~kNy|L1$353~jV3zKk3~k-4P4zK_2+~jY4y!xYHR~jlz$_~jk4z$e3X5He<0y!wy|X3[:~l|VU[F3VZ056Hy!nz/m1XD61+1XY1E1=1y|bzKiz!H034zKj~mEz#c5ZA3-3X$1~mBz$$3~lyz#,4YN5~mEz#{ZKZ3V%7Y}!J3X-YEX_J(3~mAz =V;kE0/y|F3y!}~m>z/U~mI~j_2+~mA~jp2;~m@~k32;~m>V}2u~mEX#2x~mBy+x2242(~mBy,;2242(~may->2&XkG2;~mIy-_2&NXd2;~mGz,{4<6:.:B*B:XC4>6:.>B*BBXSA+A:X]E&E<~r#z+625z s2+zN=`HXI@YMXIAXZYUM8X4K/:Q!Z&33 3YWX[~mB`{zKt4z (zV/z 3zRw2%Wd39]S11z$PAXH5Xb;ZQWU1ZgWP%3~o@{Dgl#gd}T){Uo{y5_d{e@}C(} WU9|cB{w}bzvV|)[} H|zT}d||0~{]Q|(l{|x{iv{dw}(5}[Z|kuZ }cq{{y|ij}.I{idbof%cu^d}Rj^y|-M{ESYGYfYsZslS`?ZdYO__gLYRZ&fvb4oKfhSf^d<Yeasc1f&a=hnYG{QY{D`Bsa|u,}Dl|_Q{C%xK|Aq}C>|c#ryW=}eY{L+`)][YF_Ub^h4}[X|?r|u_ex}TL@YR]j{SrXgo*|Gv|rK}B#mu{R1}hs|dP{C7|^Qt3|@P{YVV |8&}#D}ef{e/{Rl|>Hni}R1{Z#{D[}CQlQ||E}[s{SG_+i8eplY[=[|ec[$YXn#`hcm}YR|{Ci(_[ql|?8p3]-}^t{wy}4la&pc|3e{Rp{LqiJ],] `kc(]@chYnrM`O^,ZLYhZB]ywyfGY~aex!_Qww{a!|)*lHrM{N+n&YYj~Z b c#e_[hZSon|rOt`}hBXa^i{lh|<0||r{KJ{kni)|x,|0auY{D!^Sce{w;|@S|cA}Xn{C1h${E]Z-XgZ*XPbp]^_qbH^e[`YM|a||+=]!Lc}]vdBc=j-YSZD]YmyYLYKZ9Z>Xcczc2{Yh}9Fc#Z.l{}(D{G{{mRhC|L3b#|xK[Bepj#ut`H[,{E9Yr}1b{[e]{ZFk7[ZYbZ0XL]}Ye[(`d}c!|*y`Dg=b;gR]Hm=hJho}R-[n}9;{N![7k_{UbmN]rf#pTe[x8}!Qcs_rs[m`|>N}^V})7{^r|/E}),}HH{OYe2{Skx)e<_.cj.cjoMhc^d}0uYZd!^J_@g,[[[?{i@][|3S}Yl3|!1|eZ|5IYw|1D}e7|Cv{OHbnx-`wvb[6[4} =g+k:{C:}ed{S]|2M]-}WZ|/q{LF|dYu^}Gs^c{Z=}h>|/i|{W]:|ip{N:|zt|S<{DH[p_tvD{N<[8Axo{X4a.^o^X>Yfa59`#ZBYgY~_t^9`jZHZn`>G[oajZ;X,i)Z.^~YJe ZiZF^{][[#Zt^|]Fjx]&_5dddW]P0C[-]}]d|y {C_jUql] |OpaA[Z{lp|rz}:Mu#]_Yf6{Ep?f5`$[6^D][^u[$[6^.Z8]]ePc2U/=]K^_+^M{q*|9tYuZ,s(dS{i=|bNbB{uG}0jZOa:[-]dYtu3]:]<{DJ_SZIqr_`l=Yt`gkTnXb3d@kiq0a`Z{|!B|}e}Ww{Sp,^Z|0>_Z}36|]A|-t}lt{R6pi|v8hPu#{C>YOZHYmg/Z4nicK[}hF_Bg|YRZ7c|crkzYZY}_iXcZ.|)U|L5{R~qi^Uga@Y[xb}&qdbd6h5|Btw[}c<{Ds53[Y7]?Z<|e0{L[ZK]mXKZ#Z2^tavf0`PE[OSOaP`4gi`qjdYMgys/?[nc,}EEb,eL]g[n{E_b/vcvgb.{kcwi`~v%|0:|iK{Jh_vf5lb}KL|(oi=LrzhhY_^@`zgf[~g)[J_0fk_V{T)}I_{D&_/d9W/|MU[)f$xW}?$xr4<{Lb{y4}&u{XJ|cm{Iu{jQ}CMkD{CX|7A}G~{kt)nB|d5|<-}WJ}@||d@|Iy}Ts|iL|/^|no|0;}L6{Pm]7}$zf:|r2}?C_k{R(}-w|`G{Gy[g]bVje=_0|PT{^Y^yjtT[[[l!Ye_`ZN]@[n_)j3nEgMa]YtYpZy].d-Y_cjb~Y~[nc~sCi3|zg}B0}do{O^{|$`_|D{}U&|0+{J3|8*]iayx{a{xJ_9|,c{Ee]QXlYb]$[%YMc*]w[aafe]aVYi[fZEii[xq2YQZHg]Y~h#|Y:thre^@^|_F^CbTbG_1^qf7{L-`VFx Zr|@EZ;gkZ@slgko`[e}T:{Cu^pddZ_`yav^Ea+[#ZBbSbO`elQfLui}.F|txYcbQ`XehcGe~fc^RlV{D_0ZAej[l&jShxG[ipB_=u:eU}3e8[=j|{D(}dO{Do[BYUZ0/]AYE]ALYhZcYlYP/^-^{Yt_1_-;YT`P4BZG=IOZ&]H[e]YYd[9^F[1YdZxZ?Z{Z<]Ba2[5Yb[0Z4l?]d_;_)a?YGEYiYv`_XmZs4ZjY^Zb]6gqGaX^9Y}dXZr[g|]Y}K aFZp^k^F]M`^{O1Ys]ZCgCv4|E>}8eb7}l`{L5[Z_faQ|c2}Fj}hw^#|Ng|B||w2|Sh{v+[G}aB|MY}A{|8o}X~{E8paZ:]i^Njq]new)`-Z>haounWhN}c#{DfZ|fK]KqGZ=:u|fqoqcv}2ssm}.r{]{nIfV{JW)[K|,Z{Uxc|]l_KdCb%]cfobya3`p}G^|LZiSC]U|(X|kBlVg[kNo({O:g:|-N|qT}9?{MBiL}Sq{`P|3a|u.{Uaq:{_o|^S}jX{Fob0`;|#y_@[V[K|cw[<_ }KU|0F}d3|et{Q7{LuZttsmf^kYZ`Af`}$x}U`|Ww}d]| >}K,r&|XI|*e{C/a-bmr1fId4[;b>tQ_:]hk{b-pMge]gfpo.|(w[jgV{EC1Z,YhaY^q,_G[c_g[J0YX]`[h^hYK^_Yib,` {i6vf@YM^hdOKZZn(jgZ>bzSDc^Z%[[o9[2=/YHZ(_/Gu_`*|8z{DUZxYt^vuvZjhi^lc&gUd4|<UiA`z]$b/Z?l}YI^jaHxe|;F}l${sQ}5g}hA|e4}?o{ih}Uz{C)jPe4]H^J[Eg[|AMZMlc}:,{iz}#*|gc{Iq|/:|zK{l&}#u|myd{{M&v~nV};L|(g|I]ogddb0xsd7^V})$uQ{HzazsgxtsO^l}F>ZB]r|{7{j@cU^{{CbiYoHlng]f+nQ[bkTn/}<-d9q {KXadZYo+n|l[|lc}V2{[a{S4Zam~Za^`{HH{xx_SvF|ak=c^[v^7_rYT`ld@]:_ub%[$[m](Shu}G2{E.ZU_L_R{tz`vj(f?^}hswz}GdZ}{S:h`aD|?W|`dgG|if{a8|J1{N,}-Ao3{H#{mfsP|[ bzn+}_Q{MT{u4kHcj_q`eZj[8o0jy{p7}C|[}l){MuYY{|Ff!Ykn3{rT|m,^R|,R}$~Ykgx{P!]>iXh6[l[/}Jgcg{JYZ.^qYfYIZl[gZ#Xj[Pc7YyZD^+Yt;4;`e8YyZVbQ7YzZxXja.7SYl[s]2^/Ha$[6ZGYrb%XiYdf2]H]kZkZ*ZQ[ZYS^HZXcCc%Z|[(bVZ]]:OJQ_DZCg<[,]%Zaa [g{C00HY[c%[ChyZ,Z_`PbXa+eh`^&jPi0a[ggvhlekL]w{Yp^v}[e{~;k%a&k^|nR_z_Qng}[E}*Wq:{k^{FJZpXRhmh3^p>de^=_7`|ZbaAZtdhZ?n4ZL]u`9ZNc3g%[6b=e.ZVfC[ZZ^^^hD{E(9c(kyZ=bb|Sq{k`|vmr>izlH[u|e`}49}Y%}FT{[z{Rk}Bz{TCc/lMiAqkf(m$hDc;qooi[}^o:c^|Qm}a_{mrZ(pA`,}<2sY| adf_%|}`}Y5U;}/4|D>|$X{jw{C<|F.hK|*A{MRZ8Zsm?imZm_?brYWZrYx`yVZc3a@f?aK^ojEd {bN}/3ZH]/$YZhm^&j 9|(S|b]mF}UI{q&aM]LcrZ5^.|[j`T_V_Gak}9J[ ZCZD|^h{N9{~&[6Zd{}B}2O|cv]K}3s}Uy|l,fihW{EG`j_QOp~Z$F^zexS`dcISfhZBXP|.vn|_HYQ|)9|cr]<`&Z6]m_(ZhPcSg>`Z]5`~1`0Xcb4k1{O!bz|CN_T{LR|a/gFcD|j<{Z._[f)mPc:1`WtIaT1cgYkZOaVZOYFrEe[}T$}Ch}mk{K-^@]fH{Hdi`c*Z&|Kt{if[C{Q;{xYB`dYIX:ZB[}]*[{{p9|4GYRh2ao{DS|V+[zd$`F[ZXKadb*A] Ys]Maif~a/Z2bmclb8{Jro_rz|x9cHojbZ{GzZx_)]:{wAayeDlx}<=`g{H1{l#}9i|)=|lP{Qq}.({La|!Y{i2EZfp=c*}Cc{EDvVB|;g}2t{W4av^Bn=]ri,|y?|3+}T*ckZ*{Ffr5e%|sB{lx^0]eZb]9[SgAjS_D|uHZx]dive[c.YPkcq/}db{EQh&hQ|eg}G!ljil|BO]X{Qr_GkGl~YiYWu=c3eb}29v3|D|}4i||.{Mv})V{SP1{FX}CZW6{cm|vO{pS|e#}A~|1i}81|Mw}es|5[}3w{C`h9aL]o{}p[G`>i%a1Z@`Ln2bD[$_h`}ZOjhdTrH{[j_:k~kv[Sdu]CtL}41{I |[[{]Zp$]XjxjHt_eThoa#h>sSt8|gK|TVi[Y{t=}Bs|b7Zpr%{gt|Yo{CS[/{iteva|cf^hgn}($_c^wmb^Wm+|55jrbF|{9^ q6{C&c+ZKdJkq_xOYqZYSYXYl`8]-cxZAq/b%b*_Vsa[/Ybjac/OaGZ4fza|a)gY{P?| I|Y |,pi1n7}9bm9ad|=d{aV|2@[(}B`d&|Uz}B}{`q|/H|!JkM{FU|CB|.{}Az}#P|lk}K{|2rk7{^8^?`/|k>|Ka{Sq}Gz}io{DxZh[yK_#}9<{TRdgc]`~Z>JYmYJ]|`!ZKZ]gUcx|^E[rZCd`f9oQ[NcD_$ZlZ;Zr}mX|=!|$6ZPZYtIo%fj}CpcN|B,{V
+    var JCT8836=JCT11280.substring(0,8836);
+    // end of ecl.js
+
+    function getValue(value, isUnescape, isEncode) {
+        if (value === null) {
+            value = '';
+        }
+        var tmp;
+
+        tmp = (isUnescape) ? UnescapeEUCJP(value) : value;
+        tmp = (isEncode) ? encodeURIComponent(tmp) : tmp;
+
+        return tmp;
+    }
+
+    function display_step5_coupon(rtn) {
+        
+        var ad_id = '408'
+        var identifier = '45340364' //variable
+
+        var doc = document.getElementById('grp15_ias_coupon');
+        if (doc == null) {
+            return;
+        }
+
+        if (!rtn) {
+            rtn = {};
+        }
+
+        if (!rtn['seg_id']) {
+            rtn['seg_id'] = '';
+            rtn['seg_id'] = 'st5_techtest_250859';
+        }
+
+        var risp = "1";
+        if (navigator.userAgent.indexOf('IchibaApp') < 0) {
+            risp = "2";
+        }
+
+        
+
+        var runa_div = document.createElement('div');
+        div_id = "rdn-adspot-" + ad_id + "-" + identifier;
+
+        //id
+        runa_div.id = div_id
+
+        //first to insert to doc
+        doc.insertBefore(runa_div, doc.firstChild);
+
+
+        var rdn_tag_script = document.createElement('script');
+        
+        //type
+        rdn_tag_script.type = "text/javascript"
+
+
+        //innerHTML
+        rdn_tag_script.innerHTML = 'var rdntag = rdntag || {};' +
+                                    'rdntag.cmd = rdntag.cmd || [];' +
+                                    'rdntag.cmd.push(function() {' +
+                                    'rdntag.defineAd(' + ad_id + ', \'' + div_id + '\')' +
+                                    '.setResponsive(true)' +
+                                    '.setTargeting(\'xuv_i\', \'' + JSON.stringify(rtn['seg_id']) + '\')' +
+                                    '.setTargeting(\'risp\', \'' + risp + '\')'+
+                                    'rdntag.display(\'' + div_id + '\');' +
+                                    '});'
+
+
+        //append where
+        doc.insertBefore(rdn_tag_script, runa_div.nextSibling);
+
+        var cdn_script = document.createElement('script');
+        cdn_script.type = 'text/javascript';
+        cdn_script.src = "https://stg-s-cdn.rmp.rakuten.co.jp/js/aa.js";
+        cdn_script.setAttribute('async','');
+        
+        
+        doc.insertBefore(cdn_script, rdn_tag_script.nextSibling);
+
+
+    }
+    
+    function getRequestParams() {
+        var step5_params = {};
+
+        var param = grp15_ias_prm_array[0];
+        for (var key in param) {
+
+            var isUnescape = true;
+            var isEncode = false;
+            switch (key) {
+                case 'rakuten_pid':
+                case 'price':
+                case 'quantity':
+                case 'amount':
+                case 'amount_segment':
+                case 'totalamount':
+                case 'rank':
+                case 'sex':
+                case 'birth':
+                case 'card':
+                case 'shippingcost':
+                case 'payondeliverycost':
+                    isUnescape = false;
+                    break;
+                case 'shopname':
+                case 'itemname':
+                    break;
+                case 'url':
+                case 'imgurl':
+                    isEncode = true;
+                    break;
+            }
+            if (Object.prototype.toString.call(param[key]) === '[object Array]') {
+                var arrayparam = [];
+                for (var index in param[key]) {
+                    arrayparam.push(getValue(param[key][index], isUnescape, isEncode));
+                }
+                step5_params[key] = arrayparam;
+            } else {
+                step5_params[key] = getValue(param[key], isUnescape, isEncode);
+            }
+        }
+        step5_params['pageurl'] = getValue(window.location.href, false, true);
+        step5_params['pageref'] = getValue(window.document.referrer, false, true);
+        return step5_params;
+    }
+
+    function sendRequest() {
+
+/*
+        $.ajax({
+            url: '//grp15.ias.rakuten.co.jp/api?p=ichiba_step5_coupon_sp',
+            type:'GET',
+            dataType: 'jsonp',
+            data: getRequestParams(),
+            jsonpCallback: 'display_step5_coupon',
+            traditional: true,
+            timeout:3000,
+            success: function(rtn) {
+                display_step5_coupon(rtn);
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                return;
+            }
+        });
+        */
+        
+        display_step5_coupon(rtn);
+        
+    }
+    sendRequest();
+
+})(window.jQuery);
